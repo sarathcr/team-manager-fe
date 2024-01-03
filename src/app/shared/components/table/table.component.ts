dd04fc238c05a9f649d400e5d94ef315c6c5d938
scheerakkadan <scheerakkadan@opentrends.net>
2024-01-03 18:42:38 +0530

    Implemented table for timelog

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,15 +1,8 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core';
-import { TableModule } from 'primeng/table';
-import { DataList, TableDataModel } from '../../models/table-data.model';
-import { log } from 'console';
+import { Component, Input, OnInit } from '@angular/core';
 import moment from 'moment';
+import { TableModule } from 'primeng/table';
+import { TableDataModel } from '../../models/table-data.model';
+import { log } from 'console';
 
 @Component({
   selector: 'app-table',
@@ -18,35 +11,101 @@ import moment from 'moment';
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss',
 })
-export class TableComponent implements OnChanges {
+export class TableComponent implements OnInit {
   @Input() tableData!: TableDataModel;
 
-  private dateList!: any[];
-  public uniqueDates: string[] = [];
-  // public dateList
-  constructor(private cdr: ChangeDetectorRef) {}
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['tableData']) {
-      console.log(changes['tableData']);
+  public uniqueDateObjects: any[] = [];
+  public index: number = 0;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.tableData) {
       this.getDateList();
     }
   }
 
   public getDateList() {
-    this.uniqueDates = [];
+    if (!this.tableData || !this.tableData.data) {
+      return [];
+    }
 
-    this.tableData?.data?.forEach((employee) => {
-      employee.dateList.forEach((dateEntry) => {
-        const date = new Date(dateEntry?.date);
-        const formattedDate = moment(date).format('L');
+    this.uniqueDateObjects = [];
 
-        if (!this.uniqueDates.includes(formattedDate)) {
-          this.uniqueDates.push(formattedDate);
-        }
-      });
+    this.tableData.data.forEach((employee) => {
+      if (employee.dateList) {
+        employee.dateList.forEach((dateEntry) => {
+          const date = new Date(dateEntry?.date);
+          const formattedDate = moment(date).format('L');
+
+          const dateObject = {
+            date: formattedDate,
+            inTime: dateEntry.inTime,
+            outTime: dateEntry.outTime,
+            status: dateEntry.status,
+          };
+
+          const existingDateObject = this.uniqueDateObjects.find(
+            (obj) => obj.date === formattedDate
+          );
+
+          if (!existingDateObject) {
+            this.uniqueDateObjects.push(dateObject);
+          } else {
+            Object.assign(existingDateObject, dateObject);
+          }
+        });
+      }
     });
 
-    return this.uniqueDates;
+    return this.uniqueDateObjects;
+  }
+
+  public getDayAndDuration(
+    dateList: any[]
+  ): { day: string; duration: string }[] {
+    if (!dateList || dateList.length === 0) {
+      return [];
+    }
+
+    const result: { day: string; duration: string }[] = [];
+
+    dateList.forEach((dateEntry) => {
+      let dayOfWeek = '';
+      let duration = '';
+
+      if (
+        dateEntry.status === 'Present' &&
+        dateEntry.inTime &&
+        dateEntry.outTime
+      ) {
+        const inTime = moment.utc(dateEntry.inTime, 'YYYY-MM-DDTHH:mm:ss');
+        const outTime = moment.utc(dateEntry.outTime, 'YYYY-MM-DDTHH:mm:ss');
+
+        // // Check if outTime is before inTime (e.g., for the next day)
+        // if (outTime.isBefore(inTime)) {
+        //   outTime.add(1, 'day'); // Add one day to outTime
+        // }
+
+        const durationObject = moment.duration(outTime.diff(inTime));
+        const hours = durationObject.hours();
+        const minutes = durationObject.minutes();
+
+        dayOfWeek = moment(dateEntry.date).format('ddd');
+        duration = `${hours}h ${minutes}m`;
+      } else if (dateEntry.status === 'WeeklyOff') {
+        dayOfWeek = 'WeeklyOff';
+      } else if (dateEntry.status === 'Absent') {
+        dayOfWeek = 'Absent';
+      }
+
+      result.push({ day: dayOfWeek, duration: duration });
+    });
+    console.log('Result', result);
+    return result;
+  }
+
+  public incrementIndex() {
+    return ++this.index;
   }
 }
